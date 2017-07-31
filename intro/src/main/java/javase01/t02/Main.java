9bb56910d38be0a65893dfd8dc6464d3993529a2
SovietSir <sinnxronn@yandex.ru>
2017-07-31 14:21:32 +0300

    Intro task 2 solved

---
+++ intro/src/main/java/javase01/t02/Main.java
@@ -0,0 +1,15 @@
+package javase01.t02;
+
+public class Main {
+
+    private static final double eps = 0.0001;
+
+    public static void main(String[] args) {
+        int n = 1;
+        while ((1.0/((n + 1)*(n + 1))) >= eps)
+            n++;
+        System.out.printf("Number of minimal suitable member is: %d%n%n%n", n);
+        for (int i = 1; i <= n; i++)
+            System.out.printf("Member number: %03d  Member value: %f%n", i, 1.0 / ((i + 1) * (i + 1)));
+    }
+}
