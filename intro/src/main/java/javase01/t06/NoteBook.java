8d73ced6582edd9395a6e07d52dcc9f8b848ac13
SovietSir <sinnxronn@yandex.ru>
2017-08-01 12:39:04 +0300

    Intro task 6 NoteBook implemented

---
+++ intro/src/main/java/javase01/t06/NoteBook.java
@@ -0,0 +1,78 @@
+package javase01.t06;
+
+import java.util.Arrays;
+
+import static java.lang.Integer.*;
+
+public class NoteBook {
+
+    private int size;
+    private int reSize;
+    private int index = 0;
+    private NoteEntry [] NoteBook;
+
+
+
+    NoteBook(int size){
+        NoteBook = new NoteEntry[size];
+        this.size = size;
+        reSize = (int)(size * 0.1);
+    }
+
+    public void addNote(String title, String text) {
+        if (index == MAX_VALUE) {
+            System.out.println("NoteBook is full. Please, delete note first.");
+            return;
+        }
+        NoteBook[index++] = new NoteEntry(title, text);
+        System.out.println("Note successfully added!");
+        if (index == size)
+            expandNoteBook();
+    }
+
+    public void removeNote(int index) {
+        if (checkIndex (index)) {
+            for (int i = index; i < this.index; i++) {
+                NoteBook[i] = NoteBook[i + 1];
+            }
+            this.index--;
+            NoteBook[this.index] = null;
+            System.out.println("Note successfully removed!");
+        }
+    }
+
+    public void editNote(int index, String title, String text) {
+        if (checkIndex (index)) {
+            NoteBook[index].setTitle(title);
+            NoteBook[index].setText(text);
+            System.out.println("Note successfully edited!");
+        }
+    }
+
+    public void showNotes() {
+        if(index == 0) {
+            System.out.println("NoteBook is empty. Please, add note first.");
+            return;
+        }
+        for(int i = 0; i < index; i++) {
+            System.out.printf("Index: %d  %s%n", i, NoteBook[i]);
+        }
+    }
+
+    public boolean checkIndex (int index) {
+        if (index < 0 || index >= this.index) {
+            System.out.println("Note with that index not found. Please, enter correct index.");
+            return false;
+        }
+        return true;
+    }
+
+    private void expandNoteBook (){
+        if (MAX_VALUE - size > reSize)
+            size += reSize;
+        else
+            size = MAX_VALUE;
+        NoteBook = Arrays.copyOf(NoteBook, size);
+    }
+
+}
