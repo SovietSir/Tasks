cba8cc0f55cac8187e25ec6451e2be1883d8d238
SovietSir <sinnxronn@yandex.ru>
2017-08-01 10:17:54 +0300

    Intro task 6 NoteEntry implemented

---
+++ intro/src/main/java/javase01/t06/NoteEntry.java
@@ -0,0 +1,35 @@
+package javase01.t06;
+
+public class NoteEntry {
+    private String title;
+    private String text;
+
+    NoteEntry(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public String toString(){
+        return   "Note title: "
+                + getTitle()
+                + "Note text: "
+                + getText();
+    }
+}
